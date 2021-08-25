7006f4c744a2e6dba386ff61a63f1623b6db033c
mikecip10 <mikecip10@outlook.com>
2021-08-25 18:55:02 -0400

    three

--- Step1/logic.js
+++ Step1/logic.js
@@ -1,42 +1,42 @@
-// URL to earthquake json data
+// Map
+var myMap = L.map("map", {
+  center: [36.77, -119.41],
+  zoom: 5
+});
+
+// Tile
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// function to determine marker size based on magnitude
-function markerSize(magnitude) {
-    return magnitude * 5;
+// Marker
+function markerSize(quakeMagnitude) {
+    return quakeMagnitude * 5;
 }
 
-// function to return the color based on magnitude
-function markerColor(magnitude) {
-  if (magnitude > 4) {
-    return 'red'
-  } else if (magnitude > 3) {
-    return 'orange'
-  } else if (magnitude > 2) {
-    return 'yellow'
-  } else {
-    return 'green'
-  }
-}
-
-// function for opacity based on magnitude
-function markerOpacity(magnitude) {
-  if (magnitude > 6) {
-    return .99
-  } else if (magnitude > 5) {
-    return .80
-  } else if (magnitude > 4) {
-    return .70
-  } else if (magnitude > 3) {
+// Opacity
+function markerOpacity(quakeMagnitude) {
+  if (quakeMagnitude > 6) {
+    return .9
+  } else if (quakeMagnitude > 5) {
+    return .8
+  } else if (quakeMagnitude > 4) {
+    return .7
+  } else (quakeMagnitude > 3)
     return .60
-  } else if (magnitude > 2) {
-    return .50
-  } else if (magnitude > 1) {
-    return .40
-  } else {
-    return .30
   }
-}
+
+// Color
+function markerColor(quakeMagnitude) {
+  if (quakeMagnitude > 5) {
+    return 'red'
+  } else if (quakeMagnitude > 4) {
+    return 'orange'
+  } else (quakeMagnitude > 3)
+    return 'yellow'
+  }
 
 // GET request, and function to handle returned JSON data
 d3.json(queryUrl, function(data) {
@@ -66,60 +66,15 @@ function marker(feature, location) {
 
 // Define a function we want to run once for each feature in the features array
 function popup(feature, layer) {
-    // Give each feature a popup describing the place and time of the earthquake
-    return layer.bindPopup(`<h3> ${feature.properties.place} </h3> <hr> <h4>Magnitude: ${feature.properties.mag} </h4> <p> ${Date(feature.properties.time)} </p>`);
+  // Give each feature a popup describing the place and time of the earthquake
+  return layer.bindPopup(`<h3> ${feature.properties.place} </h3> <hr> <h4>Magnitude: ${feature.properties.mag} </h4> <p> ${Date(feature.properties.time)} </p>`);
 }
 
 // function to receive a layer of markers and plot them on a map.
 function createMap(earthquakes) {
 
-    // Define streetmap and darkmap layers
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
-  
-  var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-  
-    // Define a baseMaps object to hold our base layers
-    var baseMaps = {
-      "Street Map": street,
-      "Topographic Map": topo
-    };
-  
-    // Create overlay object to hold our overlay layer
-    var overlayMaps = {
-      Earthquakes: earthquakes
-    };
-  
-    // Create our map, giving it the streetmap and earthquakes layers to display on load
-    var myMap = L.map("map", {
-      center: [37.09, -95.71],
-      zoom: 4.5,
-      layers: [street, earthquakes]
-    });
-  
-    // creating the legend
-    var legend = L.control({position: 'bottomright'});
+  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
-    // add legend to map
-    legend.onAdd = function () {
-    
-        var div = L.DomUtil.create('div', 'info legend')
-        
-        div.innerHTML = "<h3>Magnitude Legend</h3><table><tr><th>>= 4</th><td>Red</td></tr><tr><th>>= 3</th><td>Orange</td></tr><tr><th>>= 2</th><td>Yellow</td></tr><tr><th>< 2</th><td>Green</td></tr></table>";
-
-        return div;
-    };
-    
-    legend.addTo(myMap);
-
-    // Create a layer control
-    // Pass in our baseMaps and overlayMaps
-    // Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
-
-  }
+}).addTo(myMap);
